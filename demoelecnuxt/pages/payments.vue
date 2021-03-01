8ca8b936da8302c7b18e04640672a453a3816b6f
Altamash219 <altamashqureshi2199@gmail.com>
2021-03-01 15:06:18 +0530

    success image added to UI

--- demoelecnuxt/pages/payments.vue
+++ demoelecnuxt/pages/payments.vue
@@ -2,7 +2,7 @@
     <div>
         <h1 class="text-center font-serif text-bold text-2xl">Scan QR code And Pay</h1>
         <img :src="require(`~/assets/${QrImage}`)" class="rounded-xl qr m-auto">
-        <primary-button action="Back" class="btnn w-56 ml-4" topage="/" id="21"></primary-button>
+        <primary-button action="Back" class="btnn w-56 ml-4" topage="/menu" id="21"></primary-button>
         <number-for-component number="22" class="float-left mt-5 ml-60"/>
     </div>
 </template>
@@ -12,6 +12,35 @@ export default {
         return{
             QrImage:'qr.png'
         }
+    },
+    mounted(){
+        var connect = new WebSocket("ws://localhost:8000")
+         setTimeout(() => {
+        this.QrImage = 'success.jpeg'
+        connect.onopen=function (event) {
+            console.log(event);
+            console.log("Successfully connected to websocket");
+            // connect.send("From javascript")
+        }
+    }, 5000);
+        // connect.onmessage=function (event) {
+        //     actionMessage=event.data
+        //     // console.log(window.location);
+        //     if (actionMessage!="Please Speak Again") {
+        //         const el = document.getElementById(`${actionMessage}`)
+        //     if(el){
+        //         el.firstChild.click()
+        //     }else{
+        //         console.log('Invalid Option')
+        //         // alert("invalid")
+        //         tempAlert("Invalid option",2000)
+        //     }
+        //     }
+
+            
+        //     console.log("Message from server:",actionMessage);
+        // }
+       
     }
 }
 </script>
